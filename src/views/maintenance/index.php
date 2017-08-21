b354249995e24f4acfa3f629c28b9e29e2522438
tofid <andreyklochok@gmail.com>
2017-08-21 19:23:31 +0300

    Fixed css

--- src/views/maintenance/index.php
+++ src/views/maintenance/index.php
@@ -1,39 +1,31 @@
-<?php
-
-
-$this->registerJs("
-var clock;
-    $(document).ready(function () {
-        clock = $('.clock').FlipClock({$minutes}, {
-        countdown: true,
-        autoStart: true
-    });
-
-    $('.start').click(function (e) {
-        clock.start();
-    });
-});
-");
-?>
-<div class="container">
-    <div class="text-center">
-        <h1>Мы скоро вернемся</h1>
-        <p class="lead">Извините, на сайте ведутся технические работы.</p>
-    </div>
-</div>
-
-<div class="curently"></div>
+<style>
+    .clock {
+        width: 460px;
+        margin: 7em auto;
+        height: 116px;
+    }
+</style>
 
 <div class="container">
-    <div class="text-center">
-        <h1>We will come back soon</h1>
-        <p class="lead">Sorry, currently we are working on this site</p>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="text-center">
+                <h1>Мы скоро вернемся</h1>
+                <p class="lead">Извините, на сайте ведутся технические работы.</p>
+            </div>
+
+            <div class="curently"></div>
+
+            <div class="text-center">
+                <h1>We will come back soon</h1>
+                <p class="lead">Sorry, currently we are working on this site</p>
+            </div>
+
+            <?php if ($minutes > 0) : ?>
+                <?php $this->registerJs("var clock; $(document).ready(function () { clock = $('.clock').FlipClock({$minutes}, { countdown: true, autoStart: true }); $('.start').click(function (e) { clock.start(); }); }); "); ?>
+                <div class="clock"></div>
+            <?php endif; ?>
+        </div>
     </div>
 </div>
-
-<?php if ($minutes > 0) : ?>
-    <div class="container">
-        <div class="clock"></div>
-    </div>
-<?php endif; ?>
 
